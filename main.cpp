b2f6a704f89fd3572bd193e718e3a1b088d613a4
Anushaswarna3 <anushaswarna3@gmail.com>
2022-03-29 00:39:43 +0530

    sample c++

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+#include <iostream>
+
+int main() {
+	using namespace std;
+    std::cout << "Hello Anuu!";
+    return 0;
+}
+
